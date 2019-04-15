ca92d7cff2f6584b7b6c76da32401b2c7212354f
Brian Biggs <loves.toys@gmail.com>
2019-04-15 07:09:11 -0400

    Initial commit

---
+++ src/com/example/idea/Main.java
@@ -0,0 +1,17 @@
+package com.example.idea;
+
+/**
+ * This is Brian's sample experiment.
+ */
+public class Main {
+
+    /**
+     * This is still Brian's main routine
+     * @param args These are the supplied command line arguments
+     */
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello from IntelliJ IDEA!");
+        System.out.println("yep, really.");
+    }
+}
